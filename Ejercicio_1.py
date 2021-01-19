89c2f34a4d14d39a8539865d68f0b14be2053155
Shary <shary2180195@correo.uis.edu.co>
2021-01-19 16:37:42 -0500

    ingreso variables

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -2,3 +2,17 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import flopy
+
+name = "tutorial01_mf6"
+h1 = 100
+h2 = 90
+Nlay = 10
+N = 101
+L = 400.0
+H = 50.0
+k = 1.0
+
+sim = flopy.mf6.MFSimulation(
+    sim_name=name, exe_name="mf6", version="mf6", sim_ws="."
+)
+
